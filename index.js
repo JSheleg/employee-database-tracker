c0ce9db73f15876f24e0de9918599a225310d9f2
JSheleg <jessica.sheleg@gmail.com>
2021-07-30 13:12:59 -0500

    switch statement

--- index.js
+++ index.js
@@ -11,4 +11,128 @@ function start() {
     loadMainPrompt();
 }
 
+function loadMainPrompt() {
+    prompt([
+        {
+            type: "list",
+            name: "choices",
+            message: "What would you like to do?",
+            choice: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "View All Employees By Department",
+                    value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+                },
+                {
+                    name: "View All Employees By Manager",
+                    value: "VIEW_EMPLOYEES_BY_MANAGER"
+                },
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Remove Employee",
+                    value: "REMOVE_EMPLOYEE"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                {
+                    name: "Update Employee Manager",
+                    value: "UPDATE_EMPLOYEE_MANAGER"
+                },
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ALL_ROLES"
+                },
+                {
+                    name: "Remove Role",
+                    value: "REMOVE_ROLE"
+                },
+                {
+                    name: "Add Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Update Role by Salary",
+                    value: "UPDATE_ROLE_BY_SALARY"
+                },
+                {
+                    name: "View Annually budgeted salary",
+                    value: "VIEW_ANNUALLY_BUDGETED_SALARY"
+                },
+                {
+                    name: "View All Departments",
+                    value: "VIEW_ALL_DEPARTMENTS"
+                },
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Remove Department",
+                    value: "REMOVE_DEPARTMENT"
+                }
+            ]
+        }
+    ]).then(res => {
+        let choice = res.choice;
+        //functions that match the users's request
+        switch(choice){
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+                break;
+            case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+                viewEmployeesByDepartment();
+                break;
+            case "VIEW_EMPLOYEES_BY_MANAGER":
+                viewEmployeesByManager();
+                break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+            case "REMOVE_EMPLOYEE":
+                removeEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_MANAGER":
+                updateEmployeeManager();
+                break;
+            case "VIEW_ALL_ROLES":
+                viewAllRoles();
+                break;
+            case "ADD_ROLE":
+                addRole();
+                break;
+            case "REMOVE_ROLE":
+                removeRole();
+                break;
+            case "UPDATE_ROLE_BY_SALARY":
+                updateRoleBySalary();
+                break;
+            case "VIEW_ANNUALLY_BUDGETED_SALARY":
+                viewAnnuallyBudgetedSalary();
+                break;
+            case "VIEW_ALL_DEPARTMENTS":
+                viewAllDepartments();
+                break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
+            case "REMOVE_DEPARTMENT":
+                removeDepartment();
+                break;
+            default:
+                quitProgram();      
+        }
+    })
+    // .then 
+}
 //function loads prompts from inquire
